ded1e345577023cf17448e9c939d9121ea5dac9b
Swastid123 <80594114+Swastid123@users.noreply.github.com>
2021-11-02 13:08:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-var canvas = new fabric.Canvas('myCanvas');
+var canvas = new fabric.Canvas("myCanvas");
  block_y=1;
  block_x=1;
 
@@ -12,11 +12,11 @@ function new_image(get_image)
 {
 	
 	
-		fabric.Image.fromURL(get.Image,function(Img){
-			block_image_object + Img;
+		fabric.Image.fromURL(get_image,function(Img){
+			block_image_object = Img;
 
 			block_image_object.scaleToWidth(block_image_width);
-			block_image_object.scaleToWidth(block_image_height);
+			block_image_object.scaleToHeight(block_image_height);
 			block_image_object.set({
 				top:block_y,
 				left:block_x
@@ -35,33 +35,33 @@ console.log(keyPressed);
 
 	if(keyPressed == '82')
 	{
-		new_image('rr1.png');
-		console.log("r");
+		new_image("rr.jpg");
+		console.log("r keyPressed");
 	}
 	if(keyPressed == '71')
 	{
 		block_x = 200;
-		new_image('gr.png');
-		console.log("g");
+		new_image("gr.png");
+		console.log("g keyPessed");
 	}
 	
 	if(keyPressed == '89')
 	{
 		block_x =350;
-		new_image('yr.png');
-		console.log("y");
+		new_image("yr.png");
+		console.log("y keyPressed");
 	}
 	if(keyPressed == '80')
 	{
 		block_x = 600;
-		new_image('pr.png');
-		console.log("p");
+		new_image("pr.png");
+		console.log("p keyPressed");
 	}
 	if(keyPressed == '66')
 	{
 		block_x = 700;
-		new_image('br.png');
-		console.log("b");
+		new_image("br.png");
+		console.log("b keyPressed");
 	}
 	
 }
